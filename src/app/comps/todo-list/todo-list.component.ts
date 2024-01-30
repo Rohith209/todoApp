96cebef4ca4d4b538734972814b355a40d2a510c
Rohith209 <rohithsai209@gmail.com>
2024-01-30 19:47:05 +0530

    filter added to table

--- src/app/comps/todo-list/todo-list.component.ts
+++ src/app/comps/todo-list/todo-list.component.ts
@@ -1,9 +1,12 @@
 import {
+  AfterContentInit,
+  AfterViewInit,
   Component,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { TodoService } from '../../services/todo.service';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,6 +14,10 @@ import { MatDialog } from '@angular/material/dialog';
 import { EditTodoComponent } from '../edit-todo/edit-todo.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ToastrService } from 'ngx-toastr';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-todo-list',
@@ -22,6 +29,10 @@ import { ToastrService } from 'ngx-toastr';
     MatButtonModule,
     JsonPipe,
     MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
   ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
@@ -35,55 +46,74 @@ export class TodoListComponent implements OnInit {
     'actions',
   ];
 
-  dataSource: [] = [];
+  dataSource!: MatTableDataSource<any>;
   isLoading: boolean = true;
-  
-  constructor(private todo: TodoService, 
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  constructor(
+    private todo: TodoService,
     private dialog: MatDialog,
-    private toastr: ToastrService) 
-  {}
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.fetchTodos();
   }
 
   fetchTodos() {
-    this.todo.getTodos().subscribe((response: any) => {
-      this.isLoading = false;
-      if(response != null) {
-        let keys = Object.keys(response);
-        response = Object.values(response);
-        response = response.map((res: any, index: number) => {
-          return { ...res, id: keys[index] };
-        });
-        this.dataSource = response;
-        this.todo.isValueChanged = false;
-      } else {
-        this.todo.isValueChanged = false;
-        return;
-      }
-      
+    this.todo.getTodos().subscribe({
+      next: (response: any) => {
+        this.isLoading = false;
+        if (response != null) {
+          let keys = Object.keys(response);
+          response = Object.values(response);
+          response = response.map((res: any, index: number) => {
+            return { ...res, id: keys[index] };
+          });
+          this.dataSource = new MatTableDataSource(response);
+          // this.dataSource.paginator = this.paginator;
+          // this.dataSource.sort = this.sort;
+          // console.log(
+          //   this.dataSource,
+          //   this.dataSource.paginator,
+          //   this.dataSource.sort
+          // );
+          this.todo.isValueChanged = false;
+        } else {
+          this.todo.isValueChanged = false;
+          return;
+        }
+      },
+      error: (error) => {
+        console.log(error);
+      },
     });
   }
 
   editTodo(todo: any) {
-    this.dialog.open(EditTodoComponent, {
-      data: todo
-    }).afterClosed().subscribe((data) => { 
-      if(data) {
-       this.updateTodo(data);
-      }
-    })
-
+    this.dialog
+      .open(EditTodoComponent, {
+        data: todo,
+      })
+      .afterClosed()
+      .subscribe((data) => {
+        if (data) {
+          this.updateTodo(data);
+        }
+      });
   }
 
-  updateTodo(todo:any) {
+  updateTodo(todo: any) {
     this.todo.updateTodo(todo).subscribe((res) => {
-      if(res) {
-        this.toastr.success("ToDo Updated!", "Success");
+      if (res) {
+        this.toastr.success('ToDo Updated!', 'Success');
         this.fetchTodos();
       }
-    })
+    });
   }
 
   deleteTodo(id: string) {
@@ -91,5 +121,14 @@ export class TodoListComponent implements OnInit {
     this.todo.deleteTodo(id).subscribe(() => {
       this.fetchTodos();
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
